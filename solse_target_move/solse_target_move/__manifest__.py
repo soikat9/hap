40d3d3df71dc7195754f5f89156f084391a5f869
AlvPerCon <104480617+AlvPerCon@users.noreply.github.com>
2022-10-14 17:26:11 -0500

    Add files via upload

---
+++ solse_target_move/solse_target_move/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019-2020 Juan Gabriel Fernandez More (kiyoshi.gf@gmail.com)
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+{
+	'name': "Asientos de destino",
+
+	'summary': """
+		Asientos de destino""",
+
+	'description': """
+		Asientos de destino
+	""",
+
+	'author': "F & M Solutions Service S.A.C",
+	'website': "http://www.solse.pe",
+
+	'category': 'Financial',
+	'version': '0.3',
+
+	'depends': [
+		'account',
+	],
+	'data': [
+		'security/ir.model.access.csv',
+		'data/tareas_auxiliares.xml',
+		'wizard/agregar_movimientos_destino.xml',
+		'views/account_account.xml',
+		'views/account_move_view.xml',
+	],
+	'auto_install': False,
+	'installable': True,
+	'application': True,
+	"sequence": 1,
+}
